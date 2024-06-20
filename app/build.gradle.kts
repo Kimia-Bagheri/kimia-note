ac9a28e77e239589e0c99ae2dd414cf5b6debd49
kimia <kimiyastudent@gmail.com>
2024-06-20 23:54:12 +0330

    second commit

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,14 +1,15 @@
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
+    id("com.google.gms.google-services")
 }
 
 android {
-    namespace = "com.kimia.mynote"
+    namespace = "com.saba.bulletjournal"
     compileSdk = 34
 
     defaultConfig {
-        applicationId = "com.kimia.mynote"
+        applicationId = "com.saba.bulletjournal"
         minSdk = 27
         targetSdk = 34
         versionCode = 1
@@ -52,16 +53,16 @@ android {
 dependencies {
 
     implementation("androidx.core:core-ktx:1.13.1")
-    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.8.2")
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.8.0")
     implementation("androidx.activity:activity-compose:1.9.0")
     implementation(platform("androidx.compose:compose-bom:2023.08.00"))
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-graphics")
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.compose.material3:material3")
-    implementation("androidx.compose.material3:material3-android:1.2.1")
-    implementation("androidx.compose.material3:material3-desktop:1.2.1")
-    implementation("androidx.compose.runtime:runtime-android:1.6.8")
+    implementation("com.google.firebase:firebase-auth:23.0.0")
+    implementation("androidx.appcompat:appcompat:1.6.1")
+    implementation("androidx.recyclerview:recyclerview:1.3.2")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
@@ -69,4 +70,7 @@ dependencies {
     androidTestImplementation("androidx.compose.ui:ui-test-junit4")
     debugImplementation("androidx.compose.ui:ui-tooling")
     debugImplementation("androidx.compose.ui:ui-test-manifest")
+
+
+
 }
